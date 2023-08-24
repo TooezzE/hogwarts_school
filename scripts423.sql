37182ec2a0b9dabf2d9c84e6e1dff8fa7e06d5ad
TooezzE <azazaz.com534@gmail.com>
2023-08-24 19:50:38 +0300

    hw 4.2

--- scripts423.sql
+++
@@ -1,8 +0,0 @@
-select student.name, student.age, faculty.name as faculty_name
-from student
-inner join faculty on student.faculty_id = faculty.id;
-
-
-select student.id as stuent_id, student.name as student_name, avatar.id as avatar_id
-from avatar
-inner join student on avatar.student_id = student.id;
